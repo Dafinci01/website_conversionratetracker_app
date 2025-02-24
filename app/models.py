49a7fbebaf3e9cf67de1d0c253246f31f11ed50c
Dafinci01 <Davehacker382gmail.com>
2025-02-24 02:01:21 +0100

    comments addded

--- app/models.py
+++ app/models.py
@@ -2,18 +2,22 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Dict, List
 
+
+#define thye  structure of an event eg visit or conversion
 class Event(BaseModel):
-    event_type: str  # "visit" or "conversion"
+    event_type: str  # type of event  "visit" or "conversion"
     timestamp: datetime
     metadata: Dict[str, str]  # Additional data (e.g., user ID, page URL)
 
+#define the structure of a setting 
 class Setting(BaseModel):
-    label: str
-    type: str
-    required: bool
+    label: str #name of the setting 
+    type: str # wtype of setting  eg text 
+    required: True
     default: str
 
+#define the structurew of the payload sent by telex
 class ConversionPayload(BaseModel):
-    channel_id: str
-    return_url: str
-    settings: List[Setting]
+    channel_id: str # id of the telex channel 
+    return_url: str # url to send results back to telex 
+    settings: List[Setting] # list of settings eg interval 
--- app/storage/memory.py
+++ app/storage/memory.py
@@ -1,10 +1,15 @@
+#in memory storage for events (replace with a database)
 events = {"visits": [], "conversions": []}
 
+
+
+#log an event (visit or conversion)
 def log_event(event: dict):
     if event["event_type"] == "visit":
         events["visits"].append(event)
     elif event["event_type"] == "conversion":
         events["conversions"].append(event)
 
+# Retrive all logged events 
 def get_events():
     return events
